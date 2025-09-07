b3f4cc830d73618bd2dd08ffb3594028f83590ce
pionas <pionas@wsieci.ws>
2025-09-07 10:15:25 +0200

    modify basicTest

--- cypress/e2e/basicTest.cy.js
+++ cypress/e2e/basicTest.cy.js
@@ -1,13 +1,6 @@
 describe('Basic Accessibility Tests', () => {
-    beforeEach(() => {
-        // Odwiedzamy stronę przed każdym testem
-        cy.visit('/')
-        // Inicjalizujemy axe-core
-        cy.injectAxe()
-    })
-
     it('Should not have any accessibility violations', () => {
-        // Uruchamiamy test dostępności
-        cy.checkA11y()
+        cy.visit('/')
+        cy.checkAccessibility()
     })
 })
